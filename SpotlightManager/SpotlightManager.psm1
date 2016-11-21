b52d7accd5c575eb29fd5217c65786c6d5fe1c8e
Aslak Tangen <aslakt@gmail.com>
2016-11-21 14:55:00 +0100

    Bugfix: Error in filename counter
    Should now properly iterate filenames "SpotlightXXX.jpg" from 000 to
    999.

--- SpotlightManager/SpotlightManager.psm1
+++ SpotlightManager/SpotlightManager.psm1
@@ -6,31 +6,38 @@
 
     $counter = 0
 
-    If (!(Test-Path $Path)) {
+    If (!(Test-Path $Path))
+	{
         New-Item $Path -ItemType Directory | Out-Null
     }
 
-    (Get-ChildItem $env:LOCALAPPDATA\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets).ForEach({
-        Copy-Item -Path $_.FullName -Destination "$Path\Spotlight$counter.jpg"
-        $counter++
-    })
+    (Get-ChildItem $env:LOCALAPPDATA\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets).ForEach(
+		{
+			Copy-Item -Path $_.FullName -Destination "$Path\Spotlight$counter.jpg"
+			$counter++
+		}
+	)
 }
 
 Function Get-FileMetaData {
     Param([string[]]$folder)
-    ForEach ($sFolder in $folder) {
+    ForEach ($sFolder in $folder)
+	{
         $a = 0
         $objShell = New-Object -ComObject Shell.Application
         $objFolder = $objShell.namespace($sFolder)
 
-        ForEach ($File in $objFolder.items()) { 
+        ForEach ($File in $objFolder.items())
+		{ 
             $FileMetaData = New-Object PSOBJECT
-            For ($a ; $a  -le 266; $a++) { 
-                    If ($objFolder.getDetailsOf($File, $a)) {
-                        $hash += @{ $($objFolder.getDetailsOf($objFolder.items, $a)) = $($objFolder.getDetailsOf($File, $a)) }
-                        $FileMetaData | Add-Member $hash
-                        $hash.clear() 
-                    }
+            For ($a ; $a  -le 266; $a++)
+			{ 
+				If ($objFolder.getDetailsOf($File, $a))
+				{
+					$hash += @{ $($objFolder.getDetailsOf($objFolder.items, $a)) = $($objFolder.getDetailsOf($File, $a)) }
+					$FileMetaData | Add-Member $hash
+					$hash.clear() 
+				}
             }
             $a=0
             $FileMetaData
@@ -45,56 +52,41 @@ Function Import-SpotlightPictures {
             [Parameter(Position = 1, Mandatory = $False)]
             [string]$Destination = "$env:USERPROFILE\OneDrive\Pictures\Spotlight"
         )
-    $Hashes = (Get-ChildItem $Destination | Get-FileHash)
-    $counter = 0
-    (Get-FileMetaData -folder $Source).ForEach({
-        If ($_.Dimensions -eq "1920 x 1080") {
-            If ($Hashes.Hash -ne $null) {
-                If (!($Hashes.Hash.Contains( (Get-FileHash $_.Path).Hash ) )) {
-                    If($counter -lt 10) {
-                        While (Test-Path "$Destination\Spotlight00$counter.jpg") {
-                            $counter++
-                        }
-                        Copy-Item $_.Path -Destination "$Destination\Spotlight00$counter.jpg"
-                        $counter++
-                    } ElseIf($counter -lt 100) {
-                        While (Test-Path "$Destination\Spotlight0$counter.jpg") {
-                            $counter++
-                        }
-                        Copy-Item $_.Path -Destination "$Destination\Spotlight0$counter.jpg"
-                        $counter++
-                    } Else {
-                        While (Test-Path "$Destination\Spotlight$counter.jpg") {
-                            $counter++
-                        }
-                        Copy-Item $_.Path -Destination "$Destination\Spotlight$counter.jpg"
-                        $counter++
-                    }
-                }
-            } Else {
-                If($counter -lt 10) {
-                    While (Test-Path "$Destination\Spotlight00$counter.jpg") {
-                        $counter++
-                    }
-                    Copy-Item $_.Path -Destination "$Destination\Spotlight00$counter.jpg"
-                    $counter++
-                } ElseIf($counter -lt 100) {
-                    While (Test-Path "$Destination\Spotlight0$counter.jpg") {
-                        $counter++
-                    }
-                    Copy-Item $_.Path -Destination "$Destination\Spotlight0$counter.jpg"
-                    $counter++
-                } Else {
-                    While (Test-Path "$Destination\Spotlight$counter.jpg") {
-                        $counter++
-                    }
-                    Copy-Item $_.Path -Destination "$Destination\Spotlight$counter.jpg"
-                    $counter++
-                }          
-            }
-        }
-    })
-
+		
+    $Hashes = (Get-ChildItem $Destination -Filter "Spotlight???.jpg" | Get-FileHash)
+    $counter = $Hashes.Count()
+	
+    (Get-FileMetaData -folder $Source).ForEach(
+		{
+			If ($_.Dimensions -eq "1920 x 1080")
+			{
+				If ($Hashes.Hash -ne $null)
+				{
+					If (!($Hashes.Hash.Contains( (Get-FileHash $_.Path).Hash ) ))
+					{
+						While (Test-Path ("$Destination\Spotlight" + $counter.ToString("000") + ".jpg"))
+						{
+							$counter++
+						}
+						Copy-Item $_.Path -Destination ("$Destination\Spotlight" + $counter.ToString("000") + ".jpg")
+						$counter++
+					}
+				}
+				Else 
+				{
+					If (!($Hashes.Hash.Contains( (Get-FileHash $_.Path).Hash ) ))
+					{
+						While (Test-Path ("$Destination\Spotlight" + $counter.ToString("000") + ".jpg"))
+						{
+							$counter++
+						}
+						Copy-Item $_.Path -Destination ("$Destination\Spotlight" + $counter.ToString("000") + ".jpg")
+						$counter++
+					}
+				}
+			}
+		}
+	)
 }
 
 Function Save-Spotlight {
@@ -107,14 +99,18 @@ Function Save-Spotlight {
         )
 
     Export-LocalSpotlightPictures -Path $TempPath
-    If (Test-Path $TempPath) {
+	
+    If (Test-Path $TempPath)
+	{
         Write-Verbose "Exported local Spotlight resources to $TempPath"
         Write-Verbose "Verifying Spotlight pictures..."
         Import-SpotlightPictures -Source $TempPath -Destination $Destination
         Write-Verbose "Imported Spotlight pictures from $TempPath to $Destination"
         Remove-Item -Path $TempPath -Recurse -Force
         Write-Host "Operation completed"
-    } Else {
+    } 
+	Else
+	{
         Throw "Error exporting local Spotlight resources to $TempPath"
     }
 }
